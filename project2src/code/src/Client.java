02ae8733869335e27fa6db94a5402b392e37ea3d
buckcm <buckcm@gmail.com>
2013-02-06 14:58:54 -0500

    Cameron: I still need to finish the console / commandliine interface which interracts with the user in UserCommands.java.

---
+++ project2src/code/src/Client.java
@@ -0,0 +1,64 @@
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public abstract class Client {
+
+	/* protected keyword is like private but subclasses have access
+	 * Socket and input/output streams
+	 */
+	protected Socket sock;				//NOTE: this requires blocking access
+	protected ObjectOutputStream output;
+	protected ObjectInputStream input;
+
+	//this is really easy
+	public boolean connect(final String server, final int port) {
+		System.out.println("attempting to connect");
+
+		/* TODO: Write this method */
+		//IS A MESSAGE NEEDED???
+		try {
+			this.sock = new Socket(server, port);
+			this.output = new ObjectOutputStream(this.sock.getOutputStream());
+			this.input = new ObjectInputStream(this.sock.getInputStream());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	//I don't really know why we need this.
+	//Plus it's easy to condense
+	public boolean isConnected() {
+		if (sock == null || !sock.isConnected()) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+	//really bad coding practice.........
+	public void disconnect()	 {
+		if (isConnected()) {
+			try
+			{
+				Envelope message = new Envelope("DISCONNECT");
+				output.writeObject(message);
+			}
+			catch(Exception e)
+			{
+				System.err.println("Error: " + e.getMessage());
+				e.printStackTrace(System.err);
+			}
+		}
+	}
+}
