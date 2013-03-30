0714080d565f068fed6e7b9ba0e1f805b61103fc
davestein73 <davestein73@localhost>
2013-03-30 15:29:22 -0400

    HMAC functionality added

---
+++ project2src/code/src/server/file/RunFileServer.java
@@ -0,0 +1,38 @@
+package server.file;
+
+import java.security.Security;
+
+/**
+ * Runs the {@link FileServer}.
+ * This class holds the main method to run the FileServer.
+ */
+public class RunFileServer
+{
+	/**
+	 * The main method.
+	 * This is the starting point for running the GroupServer.
+	 * 
+	 * @param args unused
+	 */
+	public static void main(String[] args)
+	{
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+		if (args.length > 0)
+		{
+			try
+			{
+				FileServer server = new FileServer(Integer.parseInt(args[0]));
+				server.start();
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.printf("Enter a valid port number or pass no arguments to use the default port (%d)\n", FileServer.SERVER_PORT);
+			}
+		}
+		else
+		{
+			FileServer server = new FileServer();
+			server.start();
+		}
+	}// end method main
+}// end class RunFileServer
