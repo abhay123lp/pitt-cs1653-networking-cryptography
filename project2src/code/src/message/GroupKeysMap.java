282a2ad5be3bdad4b7d5d2db4e9fe5b7bdfe4095
davestein73 <davestein73@localhost>
2013-04-04 19:39:39 -0400

    Added groupkeys

---
+++ project2src/code/src/message/GroupKeysMap.java
@@ -0,0 +1,34 @@
+package message;
+
+import java.io.Serializable;
+import java.security.Key;
+import java.util.ArrayList;
+
+
+public class GroupKeysMap implements Serializable {
+	
+	String groupName;
+	ArrayList<Key> keys;
+	
+	public GroupKeysMap(String gName, ArrayList<Key> k ){
+		this.groupName = gName;
+		this.keys = k;
+	}
+	
+	public boolean checkGroupName(String gName){
+		return this.groupName.equals(gName);
+	}
+	
+	public Key getKey(int index){
+		return keys.get(index);
+	}
+	
+	public Key getLastKey(){
+		return keys.get(keys.size() -1);
+	}
+	
+	public int getEpoch(){
+		return this.keys.size() -1;
+	}
+
+}
